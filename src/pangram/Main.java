c45d896b9a52fad364eea8e16202bb0b7547c559
ViaLimao <arrosante@gmail.com>
2025-12-25 18:10:01 -0300

    challenge: CodeWars pangram challenge

---
+++ src/pangram/Main.java
@@ -0,0 +1,31 @@
+package pangram;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* CHALLENGE
+A pangram is a sentence that contains every single letter of the alphabet at least once.
+For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram,
+because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram.
+Return True if it is, False if not. Ignore numbers and punctuation.
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(check("You shall not pass!")); // false
+        System.out.println(check("The quick brown fox jumps over the lazy dog")); // true
+    }
+
+    public static boolean check(String sentence){
+        Set<Character> letters = new HashSet<>();
+
+        for(char ch : sentence.toLowerCase().toCharArray()) {
+            if(ch >= 'a' && ch <= 'z') {
+                letters.add(ch);
+            }
+        }
+
+        return letters.size() == 26;
+    }
+}
